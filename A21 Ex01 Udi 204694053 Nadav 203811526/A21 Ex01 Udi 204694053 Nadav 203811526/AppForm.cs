c4c0d2219534313c957673b89ed9cbf0cb357d98
Nadav Zaltsman <zalts1@hotmail.com>
2020-11-09 23:49:00 +0200

    Added so far Work

---
+++ A21 Ex01 Udi 204694053 Nadav 203811526/A21 Ex01 Udi 204694053 Nadav 203811526/AppForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FacebookUI
+{
+    public partial class AppForm : Form
+    {
+        private readonly AppUI r_AppUI; 
+        public AppForm(AppUI i_AppUI)
+        {
+            r_AppUI = i_AppUI;
+            InitializeComponent();
+
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            bool loggedin = r_AppUI.login();
+            if (loggedin)
+            {
+                buttonLogin.Visible = false;
+                buttonLogin.Enabled = false;
+            }
+
+        }
+    }
+}
